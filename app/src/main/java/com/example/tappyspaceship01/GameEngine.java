34a0c0688319f4efae879522f379b6eb821ffae0
Ankita <ankitrajkaur@gmail.com>
2019-10-03 11:54:31 -0400

    initial

---
+++ app/src/main/java/com/example/tappyspaceship01/GameEngine.java
@@ -0,0 +1,222 @@
+package com.example.tappyspaceship01;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.media.Image;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameEngine extends SurfaceView implements Runnable {
+
+    // Android debug variables
+    final static String TAG="DINO-RAINBOWS";
+
+    // screen size
+    int screenHeight;
+    int screenWidth;
+
+    // game state
+    boolean gameIsRunning;
+
+    // threading
+    Thread gameThread;
+
+
+    // drawing variables
+    SurfaceHolder holder;
+    Canvas canvas;
+    Paint paintbrush;
+
+
+
+    // -----------------------------------
+    // GAME SPECIFIC VARIABLES
+    // -----------------------------------
+
+    // ----------------------------
+    // ## SPRITES
+    // ----------------------------
+
+    // represent the TOP LEFT CORNER OF THE GRAPHIC
+   //add player image
+    int playerXPosition;
+    int playerYPosition;
+    Bitmap dinoImage;
+    Rect playerHitbox;
+
+   //add candy image
+    Bitmap candyImage;
+    int candyXPosition;
+    int candyYPosition;
+    Rect candyHitbox;
+
+   //add  rainbow image
+    Bitmap rainbowImage;
+    int rainbowXPosition;
+    int rainbowYPosition;
+    Rect rainbowHitbox;
+
+    //add poop image
+    Bitmap poopImage;
+    int poopXPosition;
+    int poopYPosition;
+    Rect poopHitbox;
+
+
+    int lives = 3;
+    public GameEngine(Context context, int w, int h) {
+        super(context);
+
+        this.holder = this.getHolder();
+        this.paintbrush = new Paint();
+
+        this.screenWidth = w;
+        this.screenHeight = h;
+
+        this.printScreenInfo();
+        //@ TODO add your sprites
+        //put initial starting position of player
+        this.dinoImage = BitmapFactory.decodeResource(this.getContext().getResources(),
+                R.drawable.dino32);
+        this.playerXPosition = 1300;
+        this.playerYPosition = 120;
+    // put initial position of candy
+
+        this.candyImage = BitmapFactory.decodeResource(this.getContext().getResources(),
+                R.drawable.candy32);
+        this.candyXPosition = 1300;
+        this.candyYPosition = 120;
+
+        //put initial image for rainbow
+
+        this.rainbowImage = BitmapFactory.decodeResource(this.getContext().getResources(),
+                R.drawable.rainbow32);
+        this.rainbowXPosition = 1300;
+        this.rainbowYPosition = 120;
+        // put initial image for poop
+        this.poopImage = BitmapFactory.decodeResource(this.getContext().getResources(),
+                R.drawable.poop32);
+        this.poopXPosition = 1300;
+        this.poopYPosition = 120;
+
+    }
+
+
+
+
+    private void printScreenInfo() {
+
+        Log.d(TAG, "Screen (w, h) = " + this.screenWidth + "," + this.screenHeight);
+    }
+
+    private void spawnPlayer() {
+        //@TODO: Start the player at the left side of screen
+    }
+    private void spawnEnemyShips() {
+        Random random = new Random();
+
+        //@TODO: Place the enemies in a random location
+
+    }
+
+    // ------------------------------
+    // GAME STATE FUNCTIONS (run, stop, start)
+    // ------------------------------
+    @Override
+    public void run() {
+        while (gameIsRunning == true) {
+            this.updatePositions();
+            this.redrawSprites();
+            this.setFPS();
+        }
+    }
+
+
+    public void pauseGame() {
+        gameIsRunning = false;
+        try {
+            gameThread.join();
+        } catch (InterruptedException e) {
+            // Error
+        }
+    }
+
+    public void startGame() {
+        gameIsRunning = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+
+    // ------------------------------
+    // GAME ENGINE FUNCTIONS
+    // - update, draw, setFPS
+    // ------------------------------
+
+    public void updatePositions() {
+    }
+
+    public void redrawSprites() {
+        if (this.holder.getSurface().isValid()) {
+            this.canvas = this.holder.lockCanvas();
+
+            //----------------
+
+            // configure the drawing tools
+            this.canvas.drawColor(Color.argb(255,255,255,255));
+            paintbrush.setColor(Color.WHITE);
+
+
+            // DRAW THE PLAYER HITBOX
+            // ------------------------
+            // 1. change the paintbrush settings so we can see the hitbox
+            paintbrush.setColor(Color.BLUE);
+            paintbrush.setStyle(Paint.Style.STROKE);
+            paintbrush.setStrokeWidth(5);
+
+            //----------------
+            this.holder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    public void setFPS() {
+        try {
+            gameThread.sleep(120);
+        }
+        catch (Exception e) {
+
+        }
+    }
+
+    // ------------------------------
+    // USER INPUT FUNCTIONS
+    // ------------------------------
+
+
+    String fingerAction = "";
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int userAction = event.getActionMasked();
+        //@TODO: What should happen when person touches the screen?
+        if (userAction == MotionEvent.ACTION_DOWN) {
+
+        }
+        else if (userAction == MotionEvent.ACTION_UP) {
+
+        }
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/tappyspaceship01/Item.java
@@ -0,0 +1,74 @@
+package com.example.tappyspaceship01;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
+
+public class Item {
+
+    // PROPERTIES:
+    // Image
+    // Hitbox
+    private Bitmap image;
+    private Rect hitbox;
+
+    private int xPosition;
+    private int yPosition;
+
+    public Item(Context context, int x, int y) {
+        // 1. set up the initial position of the Enemy
+        this.xPosition = x;
+        this.yPosition = y;
+
+        // 2. Set the default image - for candy
+        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.candy32);
+        //3.set the image for rainbow
+        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.rainbow32);
+        //4.set the image for poop
+        this.image = BitmapFactory.decodeResource(context.getResources(), R.drawable.poop32);
+
+        // 3. Set the default hitbox - all enemies have same hitbox
+        this.hitbox = new Rect(
+                this.xPosition,
+                this.yPosition,
+                this.xPosition + this.image.getWidth(),
+                this.yPosition + this.image.getHeight()
+        );
+    }
+
+    // Getter and setters
+    // Autogenerate this by doing Right Click --> Generate --> Getter&Setter
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public Rect getHitbox() {
+        return hitbox;
+    }
+
+    public void setHitbox(Rect hitbox) {
+        this.hitbox = hitbox;
+    }
+
+    public int getxPosition() {
+        return xPosition;
+    }
+
+    public void setxPosition(int xPosition) {
+        this.xPosition = xPosition;
+    }
+
+    public int getyPosition() {
+        return yPosition;
+    }
+
+    public void setyPosition(int yPosition) {
+        this.yPosition = yPosition;
+    }
+}
